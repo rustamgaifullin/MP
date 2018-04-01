25655372e65310619a5e4a1dbd56d9d945f429b0
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2018-04-01 23:53:23 +0200

    integration with hockeyapp

--- .circleci/deploy_dev.sh
+++ .circleci/deploy_dev.sh
@@ -1,6 +1,6 @@
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "ipa=app/build/outputs/apk/debug/app-debug.apk" \
+-F "ipa=@app/build/outputs/apk/debug/app-debug.apk" \
 -H "X-HockeyAppToken: $HockeyAppToken" \
-https://rink.hockeyapp.net/api/2/apps/upload
+https://rink.hockeyapp.net/api/2/apps/$AndroidAppId_Dev/app_versions/upload
