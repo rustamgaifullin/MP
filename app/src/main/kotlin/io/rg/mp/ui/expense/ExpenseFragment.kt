69774f6921139ba69344b1376c5be18ac713494c
rustamgaifullin <rustam.gaifullin@gmail.com>
2019-03-14 23:25:08 +0100

    remove unused import.

--- app/src/main/kotlin/io/rg/mp/ui/expense/ExpenseFragment.kt
+++ app/src/main/kotlin/io/rg/mp/ui/expense/ExpenseFragment.kt
@@ -17,7 +17,6 @@ import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import dagger.android.support.AndroidSupportInjection
 import io.reactivex.android.schedulers.AndroidSchedulers
