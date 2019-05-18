efb13bfe89be00ef04de0edfa6c2fd4d6d73ad4c
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2019-05-18 22:03:15 +0200

    fix bug with inserting duplicated data during loading of transactions.

--- app/src/main/kotlin/io/rg/mp/persistence/dao/TransactionDao.kt
+++ app/src/main/kotlin/io/rg/mp/persistence/dao/TransactionDao.kt
@@ -12,6 +12,9 @@ interface TransactionDao {
     @Query("SELECT * FROM transactions WHERE spreadsheet_id = :spreadsheetId ORDER BY date DESC")
     fun findBySpreadsheetIdSorted(spreadsheetId: String) : Flowable<List<Transaction>>
 
+    @Query("DELETE FROM transactions WHERE spreadsheet_id = :spreadsheetId")
+    fun clearTransactions(spreadsheetId: String)
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg transactions: Transaction)
 }
